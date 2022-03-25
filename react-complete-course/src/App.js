17120d1d1409887cc63ec546f5b5221e8de047ed
Mario Krastev <mario.krastev@mentormate.com>
2022-03-25 20:30:27 +0200

    Add changes to the React course

--- react-complete-course/src/App.js
+++ react-complete-course/src/App.js
@@ -1,9 +1,39 @@
-function App() {
+// import React from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+const App = () => {
+  const expenses = [
+    {
+      title: "Car Insurance",
+      amount: 249.0,
+      date: new Date(2022, 2, 25),
+    },
+    {
+      title: "Boobs Insurance",
+      amount: 2490.0,
+      date: new Date(2022, 3, 26),
+    },
+    {
+      title: "Hause Insurance",
+      amount: 900.8,
+      date: new Date(2023, 6, 15),
+    },
+  ];
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
-}
+  // without create-react-app
+  // if import React from 'react' is included
+  //you should create DOM elements like that
+
+  /* return React.createElement(
+    "div",
+    React.createElement("h2", {}, "Let's get started!"),
+    React.createElement(Expenses, { items: expenses })
+  ); */
+};
 
 export default App;
